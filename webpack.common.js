6e19e6ea59a4327294eabe1ce1c92ca3b0571d42
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-09-02 14:48:04 +0400

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry:{
+        script: "./src/entry.ts",
+        vendor: "./src/vendor.ts"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"] // load assets as longtext
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                // Transpiles ES6-8 into ES5
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(svg|png|jpeg|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+
+            }
+        ]
+    }
+
+};
---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const common = require("./webpack.common");
+const {merge} = require("webpack-merge");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        filename: "[name].[contentHash].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    optimization:{
+        minimizer: [
+             new OptimizeCssAssetPlugin(),
+            new TerserPlugin()
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: "style.[contentHash].css"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Inject styles into DOM
+                    'css-loader', //2. Turns css into commonjs
+                    'sass-loader' //1. Turns sass into css
+                ]
+            }
+        ]
+    }
+});
+
